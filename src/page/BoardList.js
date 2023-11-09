6b3c74a5def6686335ec4ede5214c5fd4e9cac6d
jay sung <namic123@naver.com>
2023-11-09 15:22:48 +0900

    Board list 출력 코드 생성

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,42 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { Box, Spinner, Table, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import axios from "axios";
 
 export function BoardList() {
-  /* 게시판 리스트 컴포넌트 - 메인 경로 */
-  const [boardList, setBoardList] = useState(null); // 게시판 글 리스트 상태
+  const [boardList, setBoardList] = useState(null);
 
-  // 리스트 요청 코드
   useEffect(() => {
-    axios.get("api/board/list").then((response) => setBoardList(response.data));
+    axios
+      .get("/api/board/list")
+      .then((response) => setBoardList(response.data));
   }, []);
+
   return (
-    <>
+    <Box>
+      <h1>게시물 목록</h1>
       <Box>
-        <h1>게시판 목록</h1>
-        <Box>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>id</Th>
-                <Th>title</Th>
-                <Th>by</Th>
-                <Th>at</Th>
-              </Tr>
-            </Thead>
-          </Table>
-          {boardList || <Spinner />}
-          {boardList &&
-            boardList.map((board) => {
-              <Tr>
-                <Td>{board.id}</Td>
-                <Td>{board.title}</Td>
-                <Td>{board.writer}</Td>
-                <Td>{board.inserted}</Td>
-              </Tr>;
-            })}
-        </Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList === null ? (
+              <Spinner />
+            ) : (
+              boardList.map((board) => (
+                <Tr>
+                  <Td>{board.id}</Td>
+                  <Td>{board.title}</Td>
+                  <Td>{board.writer}</Td>
+                  <Td>{board.inserted}</Td>
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
       </Box>
-    </>
+    </Box>
   );
 }
