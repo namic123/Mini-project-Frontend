4b3c6f6c7cc8d537923b197bc3e00c4e485ea198
jay sung <namic123@naver.com>
2023-11-10 16:23:03 +0900

    회원 목록 링크 navigation 바에 추가

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -9,6 +9,7 @@ export function NavBar() {
       <Button onClick={() => navigate("/")}>홈</Button>
       <Button onClick={() => navigate("/write")}>글 쓰기</Button>
       <Button onClick={() => navigate("/signup")}>회원 가입</Button>
+      <Button onClick={() => navigate("/member/list")}>회원 목록</Button>
     </Flex>
   );
 }
