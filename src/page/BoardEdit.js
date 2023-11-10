6b7b07ab6545de0262678dbcea1b570b088ce0fc
jay sung <namic123@naver.com>
2023-11-10 11:17:38 +0900

    게시글 수정 시 성공 여부에 따른 응답 코드 반환

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -6,8 +6,21 @@ import {
   Input,
   Spinner,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+const { isOpen, onOpen, onClose } = useDisclosure();
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
@@ -17,6 +30,8 @@ export function BoardEdit() {
   const navigate = useNavigate();
   // /edit/:id
   const { id } = useParams();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  let toast = useToast();
 
   useEffect(() => {
     axios
@@ -29,14 +44,34 @@ export function BoardEdit() {
   }
 
   function handleSubmit() {
+    setIsSubmitting(true);
     // 저장 버튼 클릭 시
     // PUT -> /api/board/edit
     // board
     axios
       .put("/api/board/edit", board)
-      .then(() => console.log("잘됨"))
-      .catch(() => console.log)
-      .finally(() => console.log("끝"));
+      .then(() => {
+        toast({
+          description: "수정된 글이 저장되었습니다.",
+          status: "success",
+        }); // 글 수정이 성공한 경우 반환
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          //400번 오류
+          toast({
+            description: "클라이언트 오류가 발생했습니다.",
+            status: error,
+          });
+        } else {
+          toast({
+            description: "서버 오류가 발생했습니다",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => setIsSubmitting(true));
   }
 
   return (
@@ -75,9 +110,21 @@ export function BoardEdit() {
           }
         />
       </FormControl>
-      <Button colorScheme={"blue"} onClick={handleSubmit}>
-        저장
-      </Button>
+
+      {/*게시물 수정 버튼*/}
+      {/* 요청이 완료되기 전까지 로딩 */}
+      {isSubmitting === true ? (
+        <Spinner />
+      ) : (
+        <Button
+          isDisabled={isSubmitting}
+          colorScheme={"blue"}
+          onClick={handleSubmit}
+        >
+          저장
+        </Button>
+      )}
+
       <Button onClick={() => navigate(-1)}>취소</Button>
     </Box>
   );
