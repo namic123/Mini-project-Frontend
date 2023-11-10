6ee7af752cf6a7c296323897547bd478d0d4950f
jay sung <namic123@naver.com>
2023-11-10 09:28:32 +0900

    view 화면에서 수정 버튼 클릭시 수정화면(/edit/:id)로 이동

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BoardWrite } from "./page/BoardWrite";
 import { BoardList } from "./page/BoardList";
 import { HomeLayout } from "./layout/HomeLayout";
 import { BoardView } from "./page/BoardView";
+import { BoardEdit } from "./page/BoardEdit";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -16,8 +17,9 @@ const routes = createBrowserRouter(
     <Route path="/" element={<HomeLayout />}>
       <Route index element={<BoardList />} /> {/* 게시판 목록 */}
       <Route path="write" element={<BoardWrite />} /> {/* 글 작성 컴포넌트 */}
-      <Route path="board/:id" element={<BoardView />} />{" "}
       {/* 글 보기 컴포넌트 */}
+      <Route path="board/:id" element={<BoardView />} />{" "}
+      <Route path="edit/:id" element={<BoardEdit />}></Route>
     </Route>,
   ),
 );
--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -85,7 +85,9 @@ export function BoardView() {
           <Input value={board.inserted} readOnly />
         </FormControl>
 
-        <Button colorScheme={"blue"}>수정</Button>
+        <Button colorScheme={"blue"} onClick={() => navigate("/edit/" + id)}>
+          수정
+        </Button>
 
         <Button colorScheme={"red"} onClick={onOpen}>
           삭제
