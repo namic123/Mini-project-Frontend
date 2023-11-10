7c02bd611deecfea816782f476353fcb7f25a42a
jay sung <namic123@naver.com>
2023-11-10 15:14:21 +0900

    ID 중복 체크 후 toast 추가

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -6,29 +6,41 @@ import {
   FormErrorMessage,
   FormLabel,
   Input,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
 
 export function MemberSignup() {
+  // 회원 상태 관리 (id, password, passwordCheck, email))
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [email, setEmail] = useState("");
+  const toast = useToast();
+
+  // 중복 여부 체크를 위한 상태
+  // true인 경우가 중복 값이 없음을 뜻함
   const [idAvailable, setIdAvailable] = useState(false);
 
+  // 제출 조건 충족을 위한 변수
+  // true인 경우에만 제출 가능
   let submitAvailable = true;
 
+  // id가 중복된 경우
   if (!idAvailable) {
     submitAvailable = false;
   }
+  // 비밀번호 동일 여부 체크
   if (password != passwordCheck) {
     submitAvailable = false;
   }
-
+  // 비밀번호 입력 여부 체크
   if (password.length === 0) {
     submitAvailable = false;
   }
+
+  // Submit 폼
   function handleSubmit() {
     axios
       .post("/api/member/signup", {
@@ -41,23 +53,30 @@ export function MemberSignup() {
       .finally(() => console.log("done"));
   }
 
+  // ID 중복여부 체크
   function handleDuplicate() {
-    const searchParam = new URLSearchParams();
-    searchParam.set("id", id);
-    console.log(searchParam.toString());
+    const searchParam = new URLSearchParams(); // query string을 설정하는 객체
+    searchParam.set("id", id); // query string을 자동으로 encoding 해줌
 
     axios
       .get("/api/member/check?" + searchParam.toString())
       .then(() => {
         setIdAvailable(false);
+        toast({
+          description: "이미 사용중인 ID입니다.",
+          status: "warning",
+        });
       })
       .catch((error) => {
         //  존재하지 않는 아이디인 경우
         if (error.response.status === 404) {
           setIdAvailable(true);
+          toast({
+            description: "사용 가능한 ID입니다.",
+            status: "success",
+          });
         }
-      })
-      .finally(() => console.log("끝"));
+      });
   }
 
   return (
@@ -73,6 +92,7 @@ export function MemberSignup() {
               setIdAvailable(false);
             }}
           />
+          {/* 중복체크 */}
           <Button onClick={handleDuplicate}>중복 확인</Button>
         </Flex>
         <FormErrorMessage>ID 중복체크를 해주세요.</FormErrorMessage>
