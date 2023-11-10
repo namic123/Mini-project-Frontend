63cae5d110db6108b5aee68a459287c3abd49d24
jay sung <namic123@naver.com>
2023-11-10 16:21:17 +0900

    회원 목록 보기 화면 기본 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { HomeLayout } from "./layout/HomeLayout";
 import { BoardView } from "./page/board/BoardView";
 import { BoardEdit } from "./page/board/BoardEdit";
 import { MemberSignup } from "./page/member/MemberSignup";
+import { MemberList } from "./page/member/MemberList";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +23,7 @@ const routes = createBrowserRouter(
       <Route path="board/:id" element={<BoardView />} />{" "}
       <Route path="edit/:id" element={<BoardEdit />}></Route>
       <Route path="signup" element={<MemberSignup />} />
+      <Route path="mamber/list" element={<MemberList />} />
     </Route>,
   ),
 );
---
+++ src/page/member/MemberList.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+
+export function MemberList() {
+  const [memberList, setMemberList] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/api/member/list")
+      .then((response) => setMemberList(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+  if (memberList === null) {
+    return <Spinner />;
+  }
+  return (
+    <>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>pw</Th>
+              <Th>email</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {memberList.map((member) => (
+              <Tr key={member.id}>
+                <Td>{member.id}</Td>
+                <Td>{member.password}</Td>
+                <Td>{member.email}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </>
+  );
+}
