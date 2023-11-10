c3ad86bb2e449c041a8b5bd0d3061831bbb06578
jay sung <namic123@naver.com>
2023-11-10 15:40:00 +0900

    가입 후, 경로 이동 및 이메일 중복 체크 여부 기능 추가

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -10,41 +10,39 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function MemberSignup() {
-  // 회원 상태 관리 (id, password, passwordCheck, email))
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [email, setEmail] = useState("");
-  const toast = useToast();
+
+  const [idAvailable, setIdAvailable] = useState(false);
   const [emailAvailable, setEmailAvailable] = useState(false);
 
-  // 중복 여부 체크를 위한 상태
-  // true인 경우가 중복 값이 없음을 뜻함
-  const [idAvailable, setIdAvailable] = useState(false);
+  const toast = useToast();
 
-  // 제출 조건 충족을 위한 변수
-  // true인 경우에만 제출 가능
+  const navigate = useNavigate();
+
   let submitAvailable = true;
 
-  // id가 중복된 경우
-  if (!idAvailable) {
-    submitAvailable = false;
-  }
   if (!emailAvailable) {
     submitAvailable = false;
   }
-  // 비밀번호 동일 여부 체크
+
+  if (!idAvailable) {
+    submitAvailable = false;
+  }
+
   if (password != passwordCheck) {
     submitAvailable = false;
   }
-  // 비밀번호 입력 여부 체크
+
   if (password.length === 0) {
     submitAvailable = false;
   }
 
-  // Submit 폼
   function handleSubmit() {
     axios
       .post("/api/member/signup", {
@@ -52,27 +50,43 @@ export function MemberSignup() {
         password,
         email,
       })
-      .then(() => console.log("good"))
-      .catch(() => console.log("bad"))
+      .then(() => {
+        toast({
+          description: "회원가입이 완료되었습니다",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.status.status === 400) {
+          toast({
+            description: "입력값을 확인해주세요.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "가입중에 오류가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
       .finally(() => console.log("done"));
   }
 
-  // ID 중복여부 체크
-  function handleDuplicate() {
-    const searchParam = new URLSearchParams(); // query string을 설정하는 객체
-    searchParam.set("id", id); // query string을 자동으로 encoding 해줌
+  function handleIdCheck() {
+    const searchParam = new URLSearchParams();
+    searchParam.set("id", id);
 
     axios
       .get("/api/member/check?" + searchParam.toString())
       .then(() => {
         setIdAvailable(false);
         toast({
-          description: "이미 사용중인 ID입니다.",
+          description: "이미 사용 중인 ID입니다.",
           status: "warning",
         });
       })
       .catch((error) => {
-        //  존재하지 않는 아이디인 경우
         if (error.response.status === 404) {
           setIdAvailable(true);
           toast({
@@ -86,12 +100,13 @@ export function MemberSignup() {
   function handleEmailCheck() {
     const params = new URLSearchParams();
     params.set("email", email);
+
     axios
       .get("/api/member/check?" + params)
       .then(() => {
         setEmailAvailable(false);
         toast({
-          description: "이미 사용중인 email입니다.",
+          description: "이미 사용 중인 email입니다.",
           status: "warning",
         });
       })
@@ -99,7 +114,7 @@ export function MemberSignup() {
         if (error.response.status === 404) {
           setEmailAvailable(true);
           toast({
-            description: "사용 가능한 email입니다",
+            description: "사용 가능한 email입니다.",
             status: "success",
           });
         }
@@ -119,8 +134,7 @@ export function MemberSignup() {
               setIdAvailable(false);
             }}
           />
-          {/* 중복체크 */}
-          <Button onClick={handleDuplicate}>중복 확인</Button>
+          <Button onClick={handleIdCheck}>중복확인</Button>
         </Flex>
         <FormErrorMessage>ID 중복체크를 해주세요.</FormErrorMessage>
       </FormControl>
@@ -131,7 +145,8 @@ export function MemberSignup() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <FormErrorMessage>암호를 입력해주세요.</FormErrorMessage>
+
+        <FormErrorMessage>암호를 입력해 주세요.</FormErrorMessage>
       </FormControl>
       <FormControl isInvalid={password != passwordCheck}>
         <FormLabel>password 확인</FormLabel>
@@ -144,16 +159,18 @@ export function MemberSignup() {
       </FormControl>
       <FormControl isInvalid={!emailAvailable}>
         <FormLabel>email</FormLabel>
-        <Input
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setEmailAvailable(false);
-          }}
-        />
-        <Button onClick={handleEmailCheck}>중복체크</Button>
-        <FormErrorMessage>email 중복 체크를 해주세요</FormErrorMessage>
+        <Flex>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmailAvailable(false);
+              setEmail(e.target.value);
+            }}
+          />
+          <Button onClick={handleEmailCheck}>중복체크</Button>
+        </Flex>
+        <FormErrorMessage>email 중복 체크를 해주세요.</FormErrorMessage>
       </FormControl>
       <Button
         isDisabled={!submitAvailable}
