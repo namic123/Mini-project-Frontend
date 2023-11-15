0af96f99642a5f5e7d631072de76b691bd443fe1
jay sung <namic123@naver.com>
2023-11-15 11:24:09 +0900

    댓글에 새로운 줄 출력

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,7 +1,18 @@
-import { Box, Button, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 
 function CommentForm(props) {
   const [comment, setComment] = useState("");
@@ -26,7 +37,7 @@ function CommentForm(props) {
 }
 
 function CommentList(props) {
-  const [commentList, setCommentList] = useState(null);
+  const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
     const params = new URLSearchParams();
@@ -34,11 +45,32 @@ function CommentList(props) {
     axios
       .get("/api/comment/list?" + params)
       .then((response) => setCommentList(response.data));
-  }, []);
+  }, [commentList]);
 
   return (
     <>
-      <Box>댓글 리스트</Box>
+      <Card>
+        <CardHeader>
+          <Heading size={"md"}>댓글 리스트</Heading>
+        </CardHeader>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing={"4"}>
+            {/* TODO: 새로운 줄 출력 */}
+            {/* TODO: 댓글 작성 후 RE RENDER */}
+            {commentList.map((comment) => (
+              <Box>
+                <Flex justifyContent={"space-between"}>
+                  <Heading size="xs">{comment.memberId}</Heading>
+                  <Text fontSize={"xs"}>{comment.inserted}</Text>
+                </Flex>
+                <Text sx={{ whiteSpace: "pre-wrap" }} pt={"2"} fontsize={"sm"}>
+                  {comment.comment}
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        </CardBody>
+      </Card>
     </>
   );
 }
