c1c769af73b8dc536949da0760845390461193b2
jay sung <namic123@naver.com>
2023-11-19 22:35:15 +0900

    댓글 작성 시간 ago(inserted의 날짜 형식이 바뀐 메서드) 프로퍼티 사용

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -98,7 +98,7 @@ function CommentItem({
     <Box>
       <Flex justifyContent="space-between">
         <Heading size="xs">{comment.nickName}</Heading>
-        <Text fontSize="xs">{comment.inserted}</Text>
+        <Text fontSize="xs">{comment.ago}</Text>
       </Flex>
       <Flex justifyContent="space-between" alignItems="center">
         <Box flex={1}>
