b23e606152d3ae8db0a3e821d9079dd7b618a16f
jay sung <namic123@naver.com>
2023-11-22 09:55:30 +0900

    homelayout 위치 조정 및 크기 변경

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 export function HomeLayout() {
   return (
     <>
-      <Box>
+      <Box mx={{ base: 0, md: 20, lg: 40 }}>
         <NavBar /> {/* 네비게이션 바 - 공통 UI */}
         <Outlet /> {/* 하위 라우터의 컴포넌트 */}
       </Box>
